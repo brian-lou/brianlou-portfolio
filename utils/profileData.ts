bf3c88210349c43ffd5c00369f64bb12407eb57b
Brian Lou <brianlou2002@gmail.com>
2025-01-07 12:53:29 -0800

    update

--- utils/profileData.ts
+++ utils/profileData.ts
@@ -1,200 +1,223 @@
 export const profile = {
-    "main": {
-        "name": "Brian Lou",
-        "nickname": "Brian",
-        "description": "",
-        "image": "images/profpic.png",
-        "bio": "I am a senior studying Computer Science at Princeton University. I am very intellectually curious and enjoy learning about new technologies and breakthroughs in various fields. I am particularly interested in Computer Vision and Natural Language Processing, as well as the intersection of AI and Augmented Reality.",
-        "contactmessage": "",
-        "email": "brianlou2002@gmail.com",
-        "phone": "",
-        "github": "https://github.com/brianlou2024",
-        "project": "https://github.com/brian-lou",
-        "address": {
-            "street": "",
-            "city": "",
-            "state": "",
-            "zip": ""
-        },
-        "website": "https://brianlou.com",
-        "resumedownload": "https://brianlou.com",
-        "social": [
-            {
-                "name": "linkedin",
-                "url": "https://www.linkedin.com/in/brian-lou/",
-                "className": "fa fa-linkedin"
-            },
-            {
-                "name": "github",
-                "url": "https://github.com/brianlou2024",
-                "className": "fa fa-github"
-            }
-        ]
+  main: {
+    name: "Brian Lou",
+    nickname: "Brian",
+    description: "",
+    image: "images/profpic.png",
+    bio: "Hey there! I’m a developer passionate about building tools that make developers’ lives easier. At General Translation, I’m currently rethinking localization with AI to help businesses go global seamlessly. Feel free to reach out if you’d like to chat about AI, startups, or anything else!",
+    contactmessage: "",
+    email: "brian@generaltranslation.com",
+    phone: "",
+    github: "https://github.com/brian-lou",
+    project: "https://github.com/brian-lou",
+    address: {
+      street: "",
+      city: "",
+      state: "",
+      zip: "",
     },
-    "resume": {
-        "skillmessage": "",
-        "education": [
-            {
-                "school": "Princeton University",
-                "degree": "Computer Science, B.S.E.",
-                "graduated": "September 2020 - Present",
-                "gpa": "3.91/4.0",
-                "courses": {
-                    "Computer Science Courses:": "Algorithms and Data Structures, Natural Language Processing, Computer Vision, Intro to Machine Learning, Intro to Programming Systems, Advanced Programming Techniques, Economics and Computing, Computer Graphics, Human-Computer Interaction",
-                    "Math, Science, and Statistics Courses:": "Multivariable Calculus, Linear Algebra, General Physics, General Chemistry, Fundamentals of Statistics, Introduction to Data Science",
-                    "Economics and Finance Courses:": "Introduction to Microeconomics, Corporate Finance",
-                },
-                "description": []
-            },
-            {
-                "school": "University High School",
-                "degree": "High School Diploma",
-                "graduated": "September 2017 - June 2020",
-                "gpa": "4.6 Weighted",
-                "description": [
-                    "Graduated Summa Cum Laude",
-                    "National Merit Scholar"]
-            }
-        ],
-        "work": [
-            {
-                "company": "Zuora, Inc.",
-                "title": "Software Engineer Intern",
-                "years": "May 2023 - Present",
-                "description":
-                    ["Worked on the site reliability team to ensure robust CI/CD workflows and near 100% uptime of AWS instances"]
-            },
-            {
-                "company": "Princeton Computational Imaging Lab",
-                "title": "Undergraduate Researcher",
-                "years": "May 2022 - Present",
-                "description":
-                    ["Worked with Colgate-Palmolive on generating novel 3D views from real-world 2D images using Neural Networks (NeRF)",
-                        "Worked with data collection software on an autonomous driving research car sponsored by Mercedes-Benz"]
-            },
-            {
-                "company": "Alpha Origin LLC",
-                "title": "Co-Founder and Software Engineer",
-                "years": "June 2021 - Present",
-                "description":
-                    ["Manages the daily operation of this online game apps development company",
-                        "Multiple roles as a software developer, sales, marketing, and customer support",
-                        "Frontend and backend web development experience",
-                        "Products are coded in Java and downloadable as a JAR",
-                        "Company has 70,000 customers from 30+ countries",
-                        "900k+ Daily API Requests"]
-            }
-        ],
-        "skills": [
-            {
-                "name": "Python",
-                "level": "95%"
-            },
-            {
-                "name": "Java",
-                "level": "90%"
-            },
-            {
-                "name": "JavaScript/TypeScript",
-                "level": "85%"
-            },
-            {
-                "name": "React + NextJS",
-                "level": "65%"
-            },
-            {
-                "name": "Git + Linux",
-                "level": "75%"
-            },
-            {
-                "name": "HTML + CSS",
-                "level": "60%"
-            },
-            {
-                "name": "C",
-                "level": "60%"
-            },
-        ]
-    },
-    "projects": [
-        {
-            "title": "TigerSync",
-            "category": "A workout scheduling web app for Princeton athletes and coaches. Built using React and NextJS",
-            "image": "tigersync.jpg",
-            "url": "https://tigersync.uc.r.appspot.com"
-        },
-        // {
-        //     "title": "Personal Homepage",
-        //     "category": "A personal homepage built using React and NextJS",
-        //     "image": "homepage.jpg",
-        //     "url": "https://brianlou.com"
-        // },
-        // {
-        //     "title": "Skyblock Extras",
-        //     "category": "A Minecraft mod for Hypixel Skyblock, coded in Java",
-        //     "image": "sbebanner.png",
-        //     "url": "http://skyblockextras.com"
-        // },
-        {
-            "title": "Skyblock Extras Website",
-            "category": "Custom coded light-weight website for Skyblock Extras. Created with HTML, CSS, and Java Servlets",
-            "image": "sbewebsite.jpg",
-            "url": "http://skyblockextras.com"
-        },
-        {
-            "title": "Training Data Extraction Attacks on Large Language Models: A Deeper Look into GPT-2 XL, GPT-2 IMDB, and LLaMA",
-            "category": "A NLP project focused on the security vulnerabilities surrounding LLMs and their potential consequences",
-            "image": "nlpproject.png",
-            "url": "https://github.com/brianlou2024/NLPFinalProject"
-        },
-        {
-            "title": "Towards Efficient Frame Sampling Strategies for Video Action Recognition",
-            "category": "A Computer Vision project focused on investigating more efficient frame sampling strategies for the task of video action recognition",
-            "image": "cv.png",
-            "url": "https://github.com/tanushreebanerjee/cos429-final-project"
-        },
-        {
-            "title": "Maze-Man",
-            "category": "An isometric Pacman inspired game developed using Three.js",
-            "image": "mazeman.png",
-            "url": "https://github.com/brian-lou/MazeMan"
-        },
+    website: "https://brianlou.com",
+    resumedownload: "https://brianlou.com",
+    social: [
+      {
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/brian-lou/",
+        className: "fa fa-linkedin",
+      },
+      {
+        name: "github",
+        url: "https://github.com/brian-lou",
+        className: "fa fa-github",
+      },
     ],
-    "activities": [
-        {
-            "name": "Princeton University ACM",
-            "title": "Member",
-            "years": "2022 - Present",
-            "description": [
-                "Regularly attended weekly competitive programming contests",
-                "Competed at the Greater NY Regional Contest"]
+  },
+  resume: {
+    skillmessage: "",
+    education: [
+      {
+        school: "Princeton University",
+        degree: "Computer Science, B.S.E.",
+        graduated: "September 2020 - Present",
+        gpa: "3.91/4.0",
+        courses: {
+          "Computer Science Courses:":
+            "Algorithms and Data Structures, Natural Language Processing, Computer Vision, Intro to Machine Learning, Intro to Programming Systems, Advanced Programming Techniques, Economics and Computing, Computer Graphics, Human-Computer Interaction",
+          "Math, Science, and Statistics Courses:":
+            "Multivariable Calculus, Linear Algebra, General Physics, General Chemistry, Fundamentals of Statistics, Introduction to Data Science",
+          "Economics and Finance Courses:":
+            "Introduction to Microeconomics, Corporate Finance",
         },
-        {
-            "name": "Princeton University’s Men’s Swimming and Diving Team",
-            "title": "Member",
-            "years": "2020 - 2022",
-            "description": [
-                "Qualified for the 2020/2021 Tokyo Olympic Swimming Trials",
-                "2022 Scholar All-American"]
-        },
-        {
-            "name": "University High School Men’s Swimming and Diving Team",
-            "title": "Team Captain",
-            "years": "2017 - 2020",
-            "description": ["High School Student-Athlete of the year - 2020"]
-        },
-        {
-            "name": "University High School Math Club",
-            "title": "Member",
-            "years": "2017 - 2020",
-            "description": ["AIME Qualifier - 2020"]
-        },
-        {
-            "name": "University High School Physics Club",
-            "title": "Secretary",
-            "years": "2019 - 2020",
-            "description": [""]
-        },
-    ]
-
+        description: [],
+      },
+      {
+        school: "University High School",
+        degree: "High School Diploma",
+        graduated: "September 2017 - June 2020",
+        gpa: "4.6 Weighted",
+        description: ["Graduated Summa Cum Laude", "National Merit Scholar"],
+      },
+    ],
+    work: [
+      {
+        company: "General Translation",
+        title: "Founding Engineer",
+        years: "November 2024 - Present",
+        description: [""],
+      },
+      {
+        company: "Sentry.io",
+        title: "Software Engineer",
+        years: "July 2024 - November 2024",
+        description: ["Infrastructure team"],
+      },
+      {
+        company: "Alpha Origin LLC",
+        title: "Co-Founder",
+        years: "June 2021 - June 2024",
+        description: [
+          "Manages the daily operation of this online game apps development company",
+          "Multiple roles as a software developer, sales, marketing, and customer support",
+          "Frontend and backend web development experience",
+          "Products are coded in Java and downloadable as a JAR",
+          "Company has 70,000 customers from 30+ countries",
+          "900k+ Daily API Requests",
+        ],
+      },
+      {
+        company: "Princeton Computational Imaging Lab",
+        title: "Undergraduate Researcher",
+        years: "May 2022 - September 2023",
+        description: [
+          "Worked with Colgate-Palmolive on generating novel 3D views from real-world 2D images using Neural Networks (NeRF)",
+          "Worked with data collection software on an autonomous driving research car sponsored by Mercedes-Benz",
+        ],
+      },
+      {
+        company: "Zuora, Inc.",
+        title: "Software Engineer Intern",
+        years: "May 2023 - August 2023",
+        description: [
+          "Worked on the site reliability team to ensure robust CI/CD workflows and near 100% uptime of AWS instances",
+        ],
+      },
+    ],
+    skills: [
+      {
+        name: "Python",
+        level: "95%",
+      },
+      {
+        name: "Java",
+        level: "90%",
+      },
+      {
+        name: "JavaScript/TypeScript",
+        level: "85%",
+      },
+      {
+        name: "React + NextJS",
+        level: "65%",
+      },
+      {
+        name: "Git + Linux",
+        level: "75%",
+      },
+      {
+        name: "HTML + CSS",
+        level: "60%",
+      },
+      {
+        name: "C",
+        level: "60%",
+      },
+    ],
+  },
+  projects: [
+    {
+      title: "TigerSync",
+      category:
+        "A workout scheduling web app for Princeton athletes and coaches. Built using React and NextJS",
+      image: "tigersync.jpg",
+      url: "https://tigersync.uc.r.appspot.com",
+    },
+    // {
+    //     "title": "Personal Homepage",
+    //     "category": "A personal homepage built using React and NextJS",
+    //     "image": "homepage.jpg",
+    //     "url": "https://brianlou.com"
+    // },
+    // {
+    //     "title": "Skyblock Extras",
+    //     "category": "A Minecraft mod for Hypixel Skyblock, coded in Java",
+    //     "image": "sbebanner.png",
+    //     "url": "http://skyblockextras.com"
+    // },
+    {
+      title: "Skyblock Extras Website",
+      category:
+        "Custom coded light-weight website for Skyblock Extras. Created with HTML, CSS, and Java Servlets",
+      image: "sbewebsite.jpg",
+      url: "http://skyblockextras.com",
+    },
+    {
+      title:
+        "Training Data Extraction Attacks on Large Language Models: A Deeper Look into GPT-2 XL, GPT-2 IMDB, and LLaMA",
+      category:
+        "A NLP project focused on the security vulnerabilities surrounding LLMs and their potential consequences",
+      image: "nlpproject.png",
+      url: "https://github.com/brianlou2024/NLPFinalProject",
+    },
+    {
+      title:
+        "Towards Efficient Frame Sampling Strategies for Video Action Recognition",
+      category:
+        "A Computer Vision project focused on investigating more efficient frame sampling strategies for the task of video action recognition",
+      image: "cv.png",
+      url: "https://github.com/tanushreebanerjee/cos429-final-project",
+    },
+    {
+      title: "Maze-Man",
+      category: "An isometric Pacman inspired game developed using Three.js",
+      image: "mazeman.png",
+      url: "https://github.com/brian-lou/MazeMan",
+    },
+  ],
+  activities: [
+    {
+      name: "Princeton University ACM",
+      title: "Member",
+      years: "2022 - Present",
+      description: [
+        "Regularly attended weekly competitive programming contests",
+        "Competed at the Greater NY Regional Contest",
+      ],
+    },
+    {
+      name: "Princeton University’s Men’s Swimming and Diving Team",
+      title: "Member",
+      years: "2020 - 2022",
+      description: [
+        "Qualified for the 2020/2021 Tokyo Olympic Swimming Trials",
+        "2022 Scholar All-American",
+      ],
+    },
+    {
+      name: "University High School Men’s Swimming and Diving Team",
+      title: "Team Captain",
+      years: "2017 - 2020",
+      description: ["High School Student-Athlete of the year - 2020"],
+    },
+    {
+      name: "University High School Math Club",
+      title: "Member",
+      years: "2017 - 2020",
+      description: ["AIME Qualifier - 2020"],
+    },
+    {
+      name: "University High School Physics Club",
+      title: "Secretary",
+      years: "2019 - 2020",
+      description: [""],
+    },
+  ],
 };
